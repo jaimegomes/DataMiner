479db369ad0a81a02945e54c2bb9068328ecd4a5
jcgomes <jcgomes@koerich.com.br>
2017-08-01 11:24:45 -0300

    adicionando time out no client

--- DataMiner/src/main/java/br/com/mj/dataminer/client/ClientWS.java
+++ DataMiner/src/main/java/br/com/mj/dataminer/client/ClientWS.java
@@ -1,35 +1,35 @@
 package br.com.mj.dataminer.client;
 
 import java.io.File;
-import java.util.Date;
 import java.util.List;
 
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
+import br.com.mj.dataminer.model.Cliente;
+import br.com.mj.dataminer.util.Util;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
 
-import br.com.mj.dataminer.model.Cliente;
-import br.com.mj.dataminer.util.Util;
-
 /**
  * Classe que consome o WebService de captura de dados do Prestador
  * 
  * @author Jaime
- *
+ * 
  */
 public class ClientWS {
-	
-	public static List<Cliente> getInformacoesClienteWS(String cpf, File arquivoLog) {
 
+	public static List<Cliente> getInformacoesClienteWS(String cpf,
+			File arquivoLog) {
+		Client c = null;
 		try {
-			String urlws = "http://ws.consulta.plus/v2/govsc/cadastro/" + cpf
+			String urlws = "http://ws.consulta.plus/v2/govsc/cadastro/"
+					+ cpf
 					+ "?apiKey=9j8588LtIPjopyV6UwjrF12892lI45AEabt2sC7PltAyCBRAXyryiuo0iHODAE8A";
 
-			Client c = Client.create();
+			c = Client.create();
+			c.setConnectTimeout(15000);
+			c.setReadTimeout(15000);
 
 			WebResource wr = c.resource(urlws);
 
@@ -41,15 +41,20 @@ public class ClientWS {
 			}.getType());
 
 		} catch (Exception e) {
-			
+
 			System.out.println("CPF: " + cpf + " " + e.getMessage());
-			Util.escreveArquivoLog(arquivoLog , "Erro ao conectar ao WS. "+ e.getMessage());
+			Util.escreveArquivoLog(arquivoLog,
+					"Erro ao conectar ao WS. " + e.getMessage());
 
 			if (e.getMessage().indexOf("Gateway") > 0) {
 
 				System.out.println("TRATANDO ERRO, REPETINDO CPF " + cpf);
 
 				getInformacoesClienteWS(cpf, arquivoLog);
+			}
+		} finally {
+			if (c != null) {
+				c.destroy();
 			}
 		}
 		return null;
